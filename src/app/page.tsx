d3f7641da67162c0643b313212b7e7b447ad3166
Guilherme Garcia <guilhermetrianguloazul@gmail.com>
2023-07-08 02:37:42 -0300

    feat: project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
+import TripSearch from "./components/TripSearch";
+
+export default function Home() {
+  return <div>
+    <TripSearch />
+    <QuickSearch />
+    <RecommendedTrips />
+  </div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "search-background": "url(/world-map.png)",
+      },
+      colors: {
+        primary: '#590BD8',
+        primaryDarker: '#312A4F',
+        primaryLighter: '#DDD5EA',
+        grayPrimary: '#717171',
+        grayLighter: '#BBBFBF',
+        walterWhite: "#F5F5F5",
+      },
+      textColor: {
+        dark: '#717171',
+      },
+  },
+},
+  plugins: [],
+};
